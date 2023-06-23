4edea8e57bdb2e5cf205e6e12a1b41f0c4772b2c
Nentin <skorvitalik@gmail.com>
2023-06-24 00:52:35 +0300

    commit for say hello

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
+class Tank:
+    def __init__(self, model, health, armor, min_damage, max_damage):
+        self.model = model
+        self.health = health
+        self.armor = armor
+        self.damage = random.randint(min_damage, max_damage)
+
+    def print_info(self):
+        print(self.model, 'имеет', self.health, 'hp и', self.damage, 'урона')
+
+    def health_down(self, enemy_damage):
+        self.health -= enemy_damage
+        print()
+        print(self.model)
+        print('По нам попали! У нас осталось', self.health, 'hp')
+
+    def shot(self, enemy):
+        if enemy.health <= 0 or self.damage >= enemy.health :
+            enemy.health = 0
+            print(enemy.model, 'уничтожен')
+        else:
+            print()
+            print(self.model)
+            print('Есть пробитие! По', enemy.model, 'нанесено', self.damage, 'hp')
+            enemy.health_down(self.damage)
+
+class SuperTank(Tank):
+    def __init__(self, model, health, armor, min_damage, max_damage, forceArmor):
+        super().__init__(model, health, armor, min_damage, max_damage)
+        self.forceArmor = forceArmor
+
+    def health_down(self, enemy_damage):
+        super().health_down(enemy_damage//2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+tank1 = Tank('t-34', 100, 50, 10, 60)
+tank2 = Tank('Maus', 50, 10, 5, 50)
+sTank = SuperTank('BABAHA', 1000, 300, 50, 90, True)
 
+sTank.print_info()
+tank1.print_info()
+tank2.print_info()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+tank1.shot(sTank)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
